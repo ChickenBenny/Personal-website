a50131d1fb0ea7ff4c17b9aca7a8a4510b5c4ffa
ChickenBenny <zxc123benny14159@gmail.com>
2023-01-24 18:35:11 +0800

    feat: skill section

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { SiRubyonrails, SiFastapi, SiRedis, SiPostgresql, SiMysql, SiReact, SiApacheairflow, SiPytorch } from "react-icons/si";
+import { SiRubyonrails, SiFastapi, SiRedis, SiPostgresql, 
+  SiMysql, SiReact, SiApacheairflow, SiPytorch, SiLinux,
+  SiDocker, SiKeras, SiHtml5, SiCss3, SiJavascript } 
+  from "react-icons/si";
 
 const About = () => {
   return (
@@ -19,90 +22,132 @@ const About = () => {
           </p>
           <div className='border-b border-green-300 my-4 text-black' />
         </div>
-        <h2 className='py-4 text-slate-400-'>
-            I'm familiar with..
-        </h2>
-        <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>          
-          <div className='p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiRubyonrails size='3rem' />
+
+        <div>
+          <p className='py-2 text-2xl text-slate-200'>
+            I am familiar with
+          </p>
+          <h2 className='text-slate-400-'>
+            Web development
+          </h2>
+          <div className='py-2 grid grid-cols-2 lg:grid-cols-4 gap-8'>          
+            <div className='p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiRubyonrails size='3rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Ruby on Rails</h3>
+                </div>
               </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Ruby on Rails</h3>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiFastapi size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>FastAPI</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiReact size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>React</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiRedis size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Redis</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiPostgresql size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>PostgresSQL</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiMysql size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>MySQL</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiLinux size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Linux</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiDocker size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Docker</h3>
+                </div>
               </div>
             </div>
           </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiFastapi size='2rem' />
+
+          <h2 className='pt-2 text-slate-400-'>
+            Data Engineering
+          </h2>
+          <div className='py-2 grid grid-cols-2 lg:grid-cols-4 gap-8'>          
+            <div className='p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiApacheairflow size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Airflow</h3>
+                </div>
               </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>FastAPI</h3>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiPytorch size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Pytorch</h3>
+                </div>
+              </div>
+            </div>
+            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+              <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                <div className='m-auto'>
+                  <SiKeras size='2rem' />
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <h3>Keras</h3>
+                </div>
               </div>
             </div>
           </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiRedis size='2rem' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Redis</h3>
-              </div>
-            </div>
-          </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiPostgresql size='2rem' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>PostgresSQL</h3>
-              </div>
-            </div>
-          </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiMysql size='2rem' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>MySQL</h3>
-              </div>
-            </div>
-          </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiApacheairflow size='2rem' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Airflow</h3>
-              </div>
-            </div>
-          </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiPytorch size='2rem' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>PyTorch</h3>
-              </div>
-            </div>
-          </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <SiReact size='2rem' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>ReacJS</h3>
-              </div>
-            </div>
-          </div>  
         </div>
       </div>
     </div>
