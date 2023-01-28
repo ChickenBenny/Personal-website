43e2554068445261a9eae3247a255b4bb3da557b
ChickenBenny <zxc123benny14159@gmail.com>
2023-01-28 20:27:31 +0800

    feat: footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Footer = () => {
+  return (
+    <div className='w-full h-20 shadow-xl z-[100] text-center'>
+      © 2023 ALL RIGHTS RESERVED
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Experience from '@/components/Experience'
 import Project from '@/components/Project'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <About />
       <Experience />
       <Project />
+      <Footer />
     </>
   )
 }
